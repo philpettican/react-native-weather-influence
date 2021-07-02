ed12163fcb28bceb4a3e426b8801526ade96dda0
Phil Pettican <phil_pettican@hotmail.com>
2021-07-02 16:56:02 +0100

    react-native init with prettier.

---
+++ client/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	useTabs: true,
+	printWidth: 100,
+	singleQuote: true,
+	trailingComma: 'all',
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	semi: true,
+	arrowParens: 'avoid',
+};
